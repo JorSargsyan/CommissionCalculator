1993233c703dd4363dbf0378ded6f3c6c83d21a1
Jora Sargsyan <jorsargsyan@gmail.com>
2023-03-17 16:39:19 +0400

    updated initial message

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { readFileSync } from "node:fs";
 import { calculateCommissions } from "./calculations";
 
 const main = async () => {
-  console.log("Please give path of input file");
+  console.log("Please specify the path of input file");
   process.stdin.on("data", async (input) => {
     const json = JSON.parse(readFileSync(input.toString().trim(), "utf-8"));
     const result = await calculateCommissions(json);
